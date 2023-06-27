8be3f5287c48aea05978b0813d8b4c5cb4010092
Aaftab25 <shaikhaaftab2510@gmail.com>
2023-06-27 17:10:51 +0530

    node

---
+++ node-corse/first-app/main.js
@@ -0,0 +1,5 @@
+var http = require("http");
+http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+}).listen(5000);
+console.log('Server runing 5000');
